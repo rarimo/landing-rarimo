546b65d287db72ab540b2b26e9854ba9909b4cea
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2022-12-09 15:30:28 +0200

    added app-link, features section

---
+++ src/template-context/features-ctx.js
@@ -0,0 +1,21 @@
+const featuresCtx = {
+  list: [
+    {
+      icon: '/sprite.svg#icon-key',
+      title: 'features-section.security-title',
+      text: 'features-section.security-text',
+    },
+    {
+      icon: '/sprite.svg#icon-transaction-bundling',
+      title: 'features-section.transaction-bundling-title',
+      text: 'features-section.transaction-bundling-text',
+    },
+    {
+      icon: '/sprite.svg#icon-on-chain-verification',
+      title: 'features-section.on-chain-verification-title',
+      text: 'features-section.on-chain-verification-text',
+    },
+  ],
+};
+
+export default featuresCtx;
