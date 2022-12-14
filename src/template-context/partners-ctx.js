4c1d7b6f61cb194d7588024cc18fd172520f2532
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2022-12-14 11:54:34 +0200

    app bar fixes

--- src/template-context/partners-ctx.js
+++ src/template-context/partners-ctx.js
@@ -25,30 +25,6 @@ const partnersCtx = {
       icon: '/sprite.svg#icon-polygon',
       text: 'Polygon',
     },
-    {
-      icon: '/sprite.svg#icon-binance',
-      text: 'Binance',
-    },
-    {
-      icon: '/sprite.svg#icon-ethereum',
-      text: 'Ethereum',
-    },
-    {
-      icon: '/sprite.svg#icon-solana',
-      text: 'Solana',
-    },
-    {
-      icon: '/sprite.svg#icon-near',
-      text: 'Near',
-    },
-    {
-      icon: '/sprite.svg#icon-avalanche',
-      text: 'Avalanche',
-    },
-    {
-      icon: '/sprite.svg#icon-polygon',
-      text: 'Polygon',
-    },
   ],
 };
 
