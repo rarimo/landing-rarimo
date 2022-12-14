50473294cba83564d388bd1eeed697989e12b579
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2022-12-14 17:30:10 +0200

    parallax wip

--- src/main.js
+++ src/main.js
@@ -1,14 +1,47 @@
 import '@/styles/index.scss';
+import { throttle } from 'throttle-debounce';
 
 import { initLocalization } from '@/localization';
 import '@/js';
 import { hideLoader } from '@/js/init-loader';
+
+const imgRef = document.querySelector('.img-parallax');
+
+const imgParent = imgRef.parentElement;
+
+function parallaxImg() {
+  const speed = 2;
+  const imgY = imgParent.offsetTop;
+  const winY = window.scrollY;
+  const winH = window.innerHeight;
+  const parentH = imgParent.clientHeight;
+
+  // The next pixel to show on screen
+  const winBottom = winY + winH;
+
+  // If block is shown on screen
+  if (winBottom > imgY && winY < imgY + parentH) {
+    // Number of pixels shown after block appear
+    var imgBottom = (winBottom - imgY) * speed;
+    // Max number of pixels until block disappear
+    var imgTop = winH + parentH;
+    // Porcentage between start showing until disappearing
+    var imgPercent = (imgBottom / imgTop) * 100 - speed * 50;
+  }
+  imgRef.style.top = `${imgPercent}%`;
+  imgRef.style.transform = `translate(-50%, -${imgPercent}%)`;
+}
 
 const setup = () => {
   initLocalization();
 
   setTimeout(() => {
     hideLoader();
+
+    const onScroll = throttle(20, parallaxImg);
+
+    window.addEventListener('scroll', onScroll);
+    onScroll();
   }, 500);
 };
 
