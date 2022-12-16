b4f8f482804ff0acf9ed80e7ebe1264041b0d7e6
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2022-12-16 10:04:16 +0200

    fixed swiper

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -3,54 +3,49 @@ import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
 export const initSwiper = () => {
-  new Swiper('.blockchains-swiper', {
-    slidesPerView: 'auto',
-    spaceBetween: 40,
-    speed: 5000,
-    autoplay: {
-      delay: 1,
-      disableOnInteraction: false,
-    },
-    loop: true,
-    allowTouchMove: false,
-  });
-
-  new Swiper('.user-cases-swiper', {
-    initialSlide: 2,
-    centeredSlides: true,
-    slidesPerView: 1,
-    // slidesPerView: 'auto',
-    spaceBetween: 16,
-    breakpoints: {
-      900: {
-        slidesPerView: 3,
-        spaceBetween: 24,
-      },
-    },
-
-    freeMode: {
-      enabled: true,
-    },
+  const sharedOptions = {
     mousewheel: {
       forceToAxis: true,
     },
     grabCursor: true,
     edgeSwipeDetection: true,
 
-    loop: true,
-    speed: 2000,
+    freeMode: {
+      enabled: true,
+    },
+
+    rewind: true,
+    speed: 1000,
     autoplay: {
       delay: 5000,
       disableOnInteraction: false,
     },
+  };
 
-    effect: 'coverflow',
-    coverflowEffect: {
-      rotate: 50,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows: true,
+  new Swiper('.blockchains-swiper', {
+    ...sharedOptions,
+    slidesPerView: 'auto',
+    spaceBetween: 40,
+    breakpoints: {
+      900: {
+        enabled: false,
+        grabCursor: false,
+      },
+    },
+  });
+
+  new Swiper('.user-cases-swiper', {
+    ...sharedOptions,
+    slidesPerView: 1,
+    spaceBetween: 16,
+    breakpoints: {
+      800: {
+        slidesPerView: 2,
+      },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 24,
+      },
     },
   });
 };
