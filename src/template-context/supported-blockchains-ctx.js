a4617bfd17e0548b312010743fe9662baf856af0
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2022-12-21 18:56:09 +0200

    wip

--- src/template-context/supported-blockchains-ctx.js
+++ src/template-context/supported-blockchains-ctx.js
@@ -2,16 +2,16 @@ const supportedBlockchainsCtx = {
   title: 'hero-section.blockchains-title',
   list: [
     {
-      icon: '/sprite.svg#icon-binance',
-      text: 'Binance',
-    },
-    {
       icon: '/sprite.svg#icon-ethereum',
       text: 'Ethereum',
     },
     {
       icon: '/sprite.svg#icon-solana',
       text: 'Solana',
+    },
+    {
+      icon: '/sprite.svg#icon-binance',
+      text: 'Binance',
     },
     {
       icon: '/sprite.svg#icon-near',
