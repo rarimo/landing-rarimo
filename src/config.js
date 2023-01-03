15cf8beb3f3ddd2a777b30db7d8ae310465a546a
Yaroslav Panasenko <yar.panasenko@gmail.com>
2023-01-03 15:38:28 +0200

    Changed links

--- src/config.js
+++ src/config.js
@@ -2,5 +2,6 @@ export const CONFIG = {
   initLoaderDelay: 500,
   discordLink: 'https://discord.gg/Bzjm5MDXrU',
   twitterLink: 'https://twitter.com/Rarimo_protocol',
-  whitePaperLink: 'https://rarimo.gitlab.io/docs/docs/whitepaper',
+  whitePaperLink: 'https://rarimo.gitbook.io/rarimo-whitepaper/rarimo-whitepaper/abstract',
+  nftSettlementDemoLink: 'https://rarimo.gitlab.io/demo-settlement/?path=/story/demo-purchasewithanytoken--demo&full=true',
 };
