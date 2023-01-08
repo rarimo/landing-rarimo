88a446a7dfde76bb7aef8eab4e78c3a858a33d5d
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2023-01-08 18:06:11 +0200

    refactor TestnetSignUpPage

--- src/pages/TestnetSignUpPage/TestnetSignUpPage.jsx
+++ src/pages/TestnetSignUpPage/TestnetSignUpPage.jsx
@@ -1,10 +1,8 @@
 import './TestnetSignUpPage.scss';
-import { Widget } from '@typeform/embed-react'
+import { Widget } from '@typeform/embed-react';
 
 const TestnetSignUpPage = () => {
-  return (
-    <Widget id="FNmUnbCl" className="testnet-sign-up-page-form" />
-  );
+  return <Widget id="FNmUnbCl" className="testnet-sign-up-page__widget" />;
 };
 
 export default TestnetSignUpPage;
