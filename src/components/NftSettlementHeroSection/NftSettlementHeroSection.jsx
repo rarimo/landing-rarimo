45a2b558cf72104a3e821f3fc412fdebc84f1fd0
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2023-01-09 18:13:28 +0200

    added typing

--- src/components/NftSettlementHeroSection/NftSettlementHeroSection.jsx
+++ src/components/NftSettlementHeroSection/NftSettlementHeroSection.jsx
@@ -1,5 +1,6 @@
 import './NftSettlementHeroSection.scss';
 
+import { useEffect, useRef, useState } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
 import PrimaryVideoParallax from '@/components/PrimaryVideoParallax';
 import AppLink from '@/components/AppLink';
@@ -8,6 +9,63 @@ import { VIDEO_BG_COLOR } from '@/const';
 
 const NftSettlementHeroSection = () => {
   const { t } = useTranslation();
+  const [typing, setTyping] = useState('');
+  const isFirstRender = useRef(true);
+
+  const typeText = data => {
+    if (!isFirstRender.current) return;
+    isFirstRender.current = false;
+
+    let toRotate = data;
+    let loopNum = 0;
+    let period = 1500;
+    let txt = '';
+    let isDeleting = false;
+
+    const tick = () => {
+      const i = loopNum % toRotate.length;
+      const fullTxt = toRotate[i];
+
+      if (isDeleting) {
+        txt = fullTxt.substring(0, txt.length - 1);
+      } else {
+        txt = fullTxt.substring(0, txt.length + 1);
+      }
+
+      setTyping(txt);
+
+      let delta = 600 - Math.random() * 300;
+
+      if (isDeleting) {
+        delta /= 2;
+      }
+
+      if (!isDeleting && txt === fullTxt && i !== toRotate.length - 1) {
+        delta = period;
+        isDeleting = true;
+      } else if (isDeleting && !txt) {
+        isDeleting = false;
+        loopNum++;
+        delta = 500;
+      }
+
+      setTimeout(function () {
+        tick();
+      }, delta);
+    };
+
+    tick();
+  };
+
+  useEffect(() => {
+    const data = [
+      t('nft-settlement-hero-section.title-from-span'),
+      t('nft-settlement-hero-section.title-to-span'),
+    ];
+    setTimeout(() => {
+      typeText(data);
+    }, 2000);
+  }, []);
 
   return (
     <section className="nft-settlement-hero-section">
@@ -21,7 +79,7 @@ const NftSettlementHeroSection = () => {
         >
           {t('nft-settlement-hero-section.title')}
           <span className="nft-settlement-hero-section__title-span">
-            {t('nft-settlement-hero-section.title-span')}
+            {typing}
           </span>
         </h2>
         <h4
