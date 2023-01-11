1c277aa8a63369c27d16bee9d9254cb64a666966
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2023-01-11 18:55:43 +0200

    removed testnet meta tags

--- src/pages/TestnetPage/TestnetPage.jsx
+++ src/pages/TestnetPage/TestnetPage.jsx
@@ -1,4 +1,3 @@
-import MetaTags from 'react-meta-tags';
 import CommunitySection from '@/components/CommunitySection';
 import TestnetDescSection from '@/components/TestnetDescSection';
 import TestnetHeroSection from '@/components/TestnetHeroSection';
@@ -6,12 +5,6 @@ import TestnetHeroSection from '@/components/TestnetHeroSection';
 const TestnetPage = () => {
   return (
     <>
-      <MetaTags>
-        <meta property="og:image" content="/img/branding/og-testnet-img.jpg" />
-        <meta property="og:image:height" content="934" />
-        <meta property="og:image:width" content="1658" />
-        <meta name="twitter:image" content="/img/branding/og-testnet-img.jpg" />
-      </MetaTags>
       <TestnetHeroSection />
       <TestnetDescSection />
       <CommunitySection isHomePage={false} />
