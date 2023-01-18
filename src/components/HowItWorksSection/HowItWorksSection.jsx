e8b9881b59b8f8f9e5772af9962d105e24831c13
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2023-01-18 12:29:51 +0200

    wip

--- src/components/HowItWorksSection/HowItWorksSection.jsx
+++ src/components/HowItWorksSection/HowItWorksSection.jsx
@@ -1,9 +1,24 @@
 import './HowItWorksSection.scss';
 
 import { useTranslation } from 'react-i18next';
+import { useEffect, useState, useRef } from 'react';
+import cn from 'classnames';
 
 const HowItWorksSection = () => {
   const { t } = useTranslation();
+
+  const [step, setStep] = useState(1);
+
+  const isFirstRender = useRef(true);
+  // const ref = useRef(null);
+
+  useEffect(() => {
+    if (!isFirstRender.current) return;
+    isFirstRender.current = false;
+
+    // console.dir(ref.current);
+    // console.log(ref.current.getTotalLength());
+  }, []);
 
   return (
     <section className="how-it-works-section container">
@@ -32,97 +47,82 @@ const HowItWorksSection = () => {
           </li>
         </ul>
         <svg
-          className="how-it-works-section__chart-lines"
-          height="100%"
-          width="100%"
+          className={cn([
+            'svg-1 how-it-works-section__chart-lines',
+            {
+              'svg-1--animation': step >= 1,
+            },
+          ])}
+          width="492"
+          height="259"
+          viewBox="0 0 492 259"
+          xmlns="http://www.w3.org/2000/svg"
+          onAnimationEnd={() => {
+            console.log('onAnimationEnd 1');
+            setStep(2);
+          }}
         >
-          {/* <defs>
-             <linearGradient id="linear" x1="0%" y1="0%" x2="100%" y2="0%"> 
-            <linearGradient id="linear" x1="0">
-              {/* <stop offset="0%" stop-color="#05a" />
-              <stop offset="100%" stop-color="#0a5" /> 
-              <stop offset="50%" stop-color="#181B1C">
-<animate
-                  attributeName="stop-color"
-                  values="#181B1C; #181B1C; #82CFE2"
-                  dur="10s"
-                  repeatCount="indefinite"
-                ></animate>
-              </stop>
-              <stop offset="50%" stop-color="#181B1C">
-                <animate
-                  attributeName="x1"
-                  //   values="0%; 100%"
-                  from="-100%"
-                  to="100%"
-                  dur="10s"
-                  repeatCount="indefinite"
-                ></animate>
-              </stop>
-              <stop offset="50%" stop-color="#82CFE2">
-                <animate
-                  attributeName="x1"
-                  //   values="0%; 100%"
-                  from="-100%"
-                  to="100%"
-                  dur="10s"
-                  repeatCount="indefinite"
-                ></animate>
-              </stop>
-
-              <stop offset="100%" stop-color="#82CFE2">
-                 <animate
-                  attributeName="stop-color"
-                  values="#181B1C; #82CFE2; #82CFE2"
-                  dur="4s"
-                  repeatCount="indefinite"
-                ></animate> 
-              </stop>
-            </linearGradient>
-          </defs> */}
-          <path d="M10 10 H 90 V 90 H 10 Z" fill="transparent" stroke="red" />
-          {/* x1 y1, x2 y2, x y */}
           <path
-            className="how-it-works-section__chart-line"
-            d="M0 0 Q 95 10 180 80"
+            d="M0 258H204.177C215.223 258 224.177 249.046 224.177 238V142C224.177 130.954 233.131 122 244.177 122H471C482.046 122 491 113.046 491 102V20.9995C491 9.95382 482.046 0.999512 471 0.999512H254"
+            strokeWidth="2"
+            strokeLinejoin="round"
           />
-          <line
-            className="how-it-works-section__chart-line"
-            x1="17%"
-            y1="50%"
-            x2="65%"
-            y2="50%"
-            // style={{
-            //   //   stroke: 'red',
-            //   stroke: "url('#linear')",
-            //   strokeWidth: 2,
-            // }}
+        </svg>
+        <svg
+          className={cn([
+            'svg-2 how-it-works-section__chart-lines',
+            {
+              'svg-2--animation': step >= 2,
+            },
+          ])}
+          width="833"
+          height="2"
+          viewBox="0 0 833 2"
+          xmlns="http://www.w3.org/2000/svg"
+          onAnimationEnd={() => {
+            console.log('onAnimationEnd 2');
+            setStep(3);
+          }}
+        >
+          <path d="M0 1H833" strokeWidth="2" />
+        </svg>
+        <svg
+          className={cn([
+            'svg-3 how-it-works-section__chart-lines',
+            {
+              'svg-3--animation': step >= 3,
+            },
+          ])}
+          width="105"
+          height="138"
+          viewBox="0 0 105 138"
+          xmlns="http://www.w3.org/2000/svg"
+          onAnimationEnd={() => {
+            console.log('onAnimationEnd 3');
+            setStep(4);
+          }}
+        >
+          <path
+            d="M0.160156 1H32.4862C43.5319 1 52.4862 9.95431 52.4862 21V117C52.4862 128.046 61.4405 137 72.4862 137H104.812"
+            strokeWidth="2"
           />
-          {/* <circle
-            cx="30"
-            cy="40"
-            r="20"
-            // stroke="var(--col-silverpine-cyan)"
-            stroke="red"
-            stroke-width="2"
-            // stroke-dasharray="314"
-            // stroke-dashoffset="314"
-          /> */}
-          <line
-            className="how-it-works-section__chart-line"
-            x1="38%"
-            y1="93%"
-            x2="83%"
-            y2="93%"
+        </svg>
+        <svg
+          className={cn([
+            'svg-4 how-it-works-section__chart-lines',
+            {
+              'svg-4--animation': step >= 4,
+            },
+          ])}
+          width="741"
+          height="138"
+          viewBox="0 0 741 138"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M0 137H720C731.046 137 740 128.046 740 117V21C740 9.95431 731.046 1 720 1H661"
+            strokeWidth="2"
           />
-          <line
-            className="how-it-works-section__chart-line"
-            x1="38%"
-            y1="93%"
-            x2="83%"
-            y2="93%"
-          />
-          {/* <rect x="17%" y="50%" width="48%" height="20" fill="url(#linear)" /> */}
         </svg>
       </div>
     </section>
