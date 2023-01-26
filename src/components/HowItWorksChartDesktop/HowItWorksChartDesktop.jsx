caed7e3373526c5440b4b7f1f6f472eea0478a56
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2023-01-26 14:08:40 +0200

    added mobile chart

--- src/components/HowItWorksChartDesktop/HowItWorksChartDesktop.jsx
+++ src/components/HowItWorksChartDesktop/HowItWorksChartDesktop.jsx
@@ -1,9 +1,9 @@
-import './HowItWorksChart.scss';
+import './HowItWorksChartDesktop.scss';
 
 import { useEffect, useState, useRef } from 'react';
 import cn from 'classnames';
 
-const HowItWorksChart = ({
+const HowItWorksChartDesktop = ({
   isVisibleOnScreen,
   firstBlockRef,
   scaleContainer,
@@ -59,9 +59,9 @@ const HowItWorksChart = ({
     <svg
       ref={chartRef}
       className={cn([
-        'how-it-works-chart',
+        'how-it-works-chart-desktop',
         {
-          'how-it-works-chart--paused': !isVisibleOnScreen,
+          'how-it-works-chart-desktop--paused': !isVisibleOnScreen,
         },
       ])}
       width="100%"
@@ -70,15 +70,15 @@ const HowItWorksChart = ({
     >
       <path
         ref={path2BackRef}
-        className="how-it-works-chart__line-path how-it-works-chart__back-line"
+        className="how-it-works-chart-desktop__line-path how-it-works-chart-desktop__back-line"
         d="M0 1H803"
       />
       <path
         ref={path2FrontRef}
         className={cn([
-          'how-it-works-chart__path-2 how-it-works-chart__line-path how-it-works-chart__front-line',
+          'how-it-works-chart-desktop__path-2 how-it-works-chart-desktop__line-path how-it-works-chart-desktop__front-line',
           {
-            'how-it-works-chart__path-2--animation': step >= 2,
+            'how-it-works-chart-desktop__path-2--animation': step >= 2,
           },
         ])}
         d="M0 1H803"
@@ -87,15 +87,15 @@ const HowItWorksChart = ({
 
       <path
         ref={path1BackRef}
-        className="how-it-works-chart__line-path how-it-works-chart__front-line"
+        className="how-it-works-chart-desktop__line-path how-it-works-chart-desktop__front-line"
         d="M0 258H216.652C228.373 258 237.874 249.046 237.874 238V142C237.874 130.954 247.375 122 259.096 122H499.778C511.499 122 521 113.046 521 102V20.9995C521 9.95382 511.499 0.999512 499.778 0.999512H269.519"
       />
       <path
         ref={path1FrontRef}
         className={cn([
-          'how-it-works-chart__path-1 how-it-works-chart__line-path how-it-works-chart__back-line',
+          'how-it-works-chart-desktop__path-1 how-it-works-chart-desktop__line-path how-it-works-chart-desktop__back-line',
           {
-            'how-it-works-chart__path-1--animation': step >= 1,
+            'how-it-works-chart-desktop__path-1--animation': step >= 1,
           },
         ])}
         d="M0 258H216.652C228.373 258 237.874 249.046 237.874 238V142C237.874 130.954 247.375 122 259.096 122H499.778C511.499 122 521 113.046 521 102V20.9995C521 9.95382 511.499 0.999512 499.778 0.999512H269.519"
@@ -104,15 +104,15 @@ const HowItWorksChart = ({
 
       <path
         ref={path3BackRef}
-        className="how-it-works-chart__line-path how-it-works-chart__back-line"
+        className="how-it-works-chart-desktop__line-path how-it-works-chart-desktop__back-line"
         d="M0.160156 1H32.4862C43.5319 1 52.4862 9.95431 52.4862 21V117C52.4862 128.046 61.4405 137 72.4862 137H104.812"
       />
       <path
         ref={path3FrontRef}
         className={cn([
-          'how-it-works-chart__path-3 how-it-works-chart__line-path how-it-works-chart__front-line',
+          'how-it-works-chart-desktop__path-3 how-it-works-chart-desktop__line-path how-it-works-chart-desktop__front-line',
           {
-            'how-it-works-chart__path-3--animation': step >= 3,
+            'how-it-works-chart-desktop__path-3--animation': step >= 3,
           },
         ])}
         d="M0.160156 1H32.4862C43.5319 1 52.4862 9.95431 52.4862 21V117C52.4862 128.046 61.4405 137 72.4862 137H104.812"
@@ -121,15 +121,15 @@ const HowItWorksChart = ({
 
       <path
         ref={path4BackRef}
-        className="how-it-works-chart__line-path how-it-works-chart__front-line"
+        className="how-it-works-chart-desktop__line-path how-it-works-chart-desktop__front-line"
         d="M0 137H720C731.046 137 740 128.046 740 117V21C740 9.95431 731.046 1 720 1H661"
       />
       <path
         ref={path4FrontRef}
         className={cn([
-          'how-it-works-chart__path-4 how-it-works-chart__line-path how-it-works-chart__back-line',
+          'how-it-works-chart-desktop__path-4 how-it-works-chart-desktop__line-path how-it-works-chart-desktop__back-line',
           {
-            'how-it-works-chart__path-4--animation': step >= 4,
+            'how-it-works-chart-desktop__path-4--animation': step >= 4,
           },
         ])}
         d="M0 137H720C731.046 137 740 128.046 740 117V21C740 9.95431 731.046 1 720 1H661"
@@ -138,4 +138,4 @@ const HowItWorksChart = ({
   );
 };
 
-export default HowItWorksChart;
+export default HowItWorksChartDesktop;
