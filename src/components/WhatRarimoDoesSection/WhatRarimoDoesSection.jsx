a23005872f3bb8a4b1bac052d5685b9667725f55
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2023-03-30 11:32:28 +0300

    added books png

--- src/components/WhatRarimoDoesSection/WhatRarimoDoesSection.jsx
+++ src/components/WhatRarimoDoesSection/WhatRarimoDoesSection.jsx
@@ -15,31 +15,33 @@ const WhatRarimoDoesSection = () => {
         <h6 className="what-rarimo-does-section__subtitle">
           {t('what-rarimo-does-section.subtitle')}
         </h6>
-        <div className="what-rarimo-does-section__title-wrapper">
-          <h3 className="what-rarimo-does-section__title">
-            {t('what-rarimo-does-section.title')}
-          </h3>
-          <p className="what-rarimo-does-section__description">
-            {t('what-rarimo-does-section.description')}
-          </p>
+        <div className="what-rarimo-does-section__main-content">
+          <div className="what-rarimo-does-section__title-wrapper">
+            <h3 className="what-rarimo-does-section__title">
+              {t('what-rarimo-does-section.title')}
+            </h3>
+            <p className="what-rarimo-does-section__description">
+              {t('what-rarimo-does-section.description')}
+            </p>
+          </div>
+          <ul className="what-rarimo-does-section__list">
+            {whatRarimoDoesSectionList.map((item, index) => (
+              <li className="what-rarimo-does-section__list-item" key={index}>
+                <div className="what-rarimo-does-section__list-item-icon">
+                  <svg height="24" width="24">
+                    <use href={item.icon}></use>
+                  </svg>
+                </div>
+                <h6 className="what-rarimo-does-section__list-item-title">
+                  {t(item.titleKey)}
+                </h6>
+                <p className="what-rarimo-does-section__list-item-text">
+                  {t(item.textKey)}
+                </p>
+              </li>
+            ))}
+          </ul>
         </div>
-        <ul className="what-rarimo-does-section__list">
-          {whatRarimoDoesSectionList.map((item, index) => (
-            <li className="what-rarimo-does-section__list-item" key={index}>
-              <div className="what-rarimo-does-section__list-item-icon">
-                <svg height="24" width="24">
-                  <use href={item.icon}></use>
-                </svg>
-              </div>
-              <h6 className="what-rarimo-does-section__list-item-title">
-                {t(item.titleKey)}
-              </h6>
-              <p className="what-rarimo-does-section__list-item-text">
-                {t(item.textKey)}
-              </p>
-            </li>
-          ))}
-        </ul>
       </section>
     </SectionWrapper>
   );
