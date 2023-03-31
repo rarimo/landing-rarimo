bf61243b1aea4ab536b2515c838695df848b34ba
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2023-03-31 16:27:47 +0300

    backers section

--- src/template-data/backers.data.js
+++ src/template-data/backers.data.js
@@ -1,23 +1,18 @@
 export const backersList = [
   {
-    image: '/img/backers/pantera.webp',
-    width: 78,
-    height: 23,
-  },
-  {
-    image: '/img/backers/pareto.webp',
+    image: '/img/backers/pareto.png',
     width: 69,
     height: 10,
   },
   {
-    image: '/img/backers/eniqa.webp',
-    width: 53,
-    height: 17,
-  },
-  {
-    image: '/img/backers/greycroft.webp',
+    image: '/img/backers/greycroft.png',
     width: 72,
     height: 16,
+  },
+  {
+    image: '/img/backers/eniqa.png',
+    width: 53,
+    height: 17,
   },
   {
     image: '/img/backers/hyper.png',
@@ -25,12 +20,12 @@ export const backersList = [
     height: 12,
   },
   {
-    image: '/img/backers/circle.webp',
+    image: '/img/backers/circle.png',
     width: 69,
     height: 21,
   },
   {
-    image: '/img/backers/protocol-labs.webp',
+    image: '/img/backers/protocol-labs.png',
     width: 91,
     height: 24,
   },
