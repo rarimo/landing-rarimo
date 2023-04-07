62a5d44471e3384aede46baf3ee3aaff97342418
Semen Loktionov <loktionov.kh@gmail.com>
2023-04-07 17:24:47 +0300

    add hubspot

---
+++ src/hubspot-api.js
@@ -0,0 +1,10 @@
+import { Fetcher } from '@distributedlab/fetcher';
+import { CONFIG } from '@/config';
+
+export const hubspotApi = new Fetcher({
+  baseUrl: CONFIG.hubspotProxyUrl,
+  headers: {
+    'Content-Type': 'application/json',
+    "Origin": window.origin,
+  },
+})
