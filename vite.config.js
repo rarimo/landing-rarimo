5ee69d42cc6a8eb5c938d5b5f548eb9fae3cf999
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2023-04-28 12:00:26 +0300

    added eslint parser alias

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import * as fs from 'fs';
 import * as path from 'path';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 const appDirectory = fs.realpathSync(process.cwd());
 const resolveApp = relative => path.resolve(appDirectory, relative);
