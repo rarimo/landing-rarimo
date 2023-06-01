eb2523f1918c541c1d99a3ca47d8e49c8c268522
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2023-06-01 13:43:19 +0300

    footer

--- src/components/AppFooter/AppFooter.jsx
+++ src/components/AppFooter/AppFooter.jsx
@@ -1,45 +1,61 @@
 import './AppFooter.scss';
 
+import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 
 import AppButton, { APP_BUTTON_SCHEMES } from '@/components/AppButton';
-import SectionWrapper from '@/components/SectionWrapper';
 import { CONFIG } from '@/config';
 import { ROUTES_PATHS } from '@/const';
+import { handleNavClick } from '@/helpers';
+import { navigation } from '@/template-data';
 
 const AppFooter = () => {
   const { t } = useTranslation();
+
+  // Exclude home page link
+  const navList = useMemo(() => navigation.filter((_, index) => index), []);
 
   const onScrollToTop = () => {
     window.scrollTo({ top: 0 });
   };
 
   return (
-    <SectionWrapper className="app-footer">
-      <div className="app-footer__wrapper container">
-        <table className="app-footer__bg-table">
-          <tbody>
-            {Array(4)
-              .fill(null)
-              .map((_, i) => (
-                <tr key={i}>
-                  <td className="app-footer__bg-table-cell" />
-                </tr>
-              ))}
-          </tbody>
-        </table>
-        <footer className="app-footer__content">
-          <div className="app-footer__logo-wrapper" data-aos="fade-up">
-            <Link className="app-footer__logo" to={ROUTES_PATHS.home}>
-              <svg className="app-footer__logo-img" height="24" width="93">
-                <use href="/icons/sprite.svg#icon-app-logo"></use>
-              </svg>
-            </Link>
-            <span>&copy; {new Date().getFullYear()}</span>
-          </div>
-          {/* <div className="app-footer__navigation"></div> */}
-          <div className="app-footer__social-links-wrapper" data-aos="fade-up">
+    <footer className="app-footer">
+      <div className="app-footer__content container" data-aos="fade-up">
+        <div className="app-footer__logo-wrapper">
+          <Link className="app-footer__logo" to={ROUTES_PATHS.home}>
+            <svg className="app-footer__logo-img" height="24" width="93">
+              <use href="/icons/sprite.svg#icon-app-logo"></use>
+            </svg>
+          </Link>
+          <span>&copy; {new Date().getFullYear()}</span>
+        </div>
+        <ul className="app-footer__links-list">
+          {navList.map((link, index) => (
+            <li
+              className="app-footer__link"
+              key={index}
+              role="link"
+              tabIndex="0"
+              onClick={() => handleNavClick(link)}
+              onKeyDown={event => {
+                switch (event.code) {
+                  case 'Enter':
+                    handleNavClick(link);
+                    return;
+
+                  default:
+                    return;
+                }
+              }}
+            >
+              {t(link.textKey)}
+            </li>
+          ))}
+        </ul>
+        <ul className="app-footer__links-list">
+          <li>
             <a
               className="app-footer__link"
               href={CONFIG.discordLink}
@@ -51,6 +67,8 @@ const AppFooter = () => {
                 <use href="/icons/sprite.svg#icon-arrow-right"></use>
               </svg>
             </a>
+          </li>
+          <li>
             <a
               className="app-footer__link"
               href={CONFIG.twitterLink}
@@ -62,6 +80,8 @@ const AppFooter = () => {
                 <use href="/icons/sprite.svg#icon-arrow-right"></use>
               </svg>
             </a>
+          </li>
+          <li>
             <a
               className="app-footer__link"
               href={CONFIG.telegramLink}
@@ -73,25 +93,24 @@ const AppFooter = () => {
                 <use href="/icons/sprite.svg#icon-arrow-right"></use>
               </svg>
             </a>
-          </div>
-          <div className="app-footer__space" />
-          <AppButton
-            className="app-footer__scroll-to-top-btn"
-            scheme={APP_BUTTON_SCHEMES.secondary}
-            onClick={onScrollToTop}
-            data-aos="fade-up"
+          </li>
+        </ul>
+        <div className="app-footer__space" />
+        <AppButton
+          className="app-footer__scroll-to-top-btn"
+          scheme={APP_BUTTON_SCHEMES.secondary}
+          onClick={onScrollToTop}
+        >
+          <svg
+            className="app-footer__scroll-to-top-icon"
+            height="14"
+            width="14"
           >
-            <svg
-              className="app-footer__scroll-to-top-icon"
-              height="16"
-              width="16"
-            >
-              <use href="/icons/sprite.svg#icon-arrow-right"></use>
-            </svg>
-          </AppButton>
-        </footer>
+            <use href="/icons/sprite.svg#icon-arrow-right"></use>
+          </svg>
+        </AppButton>
       </div>
-    </SectionWrapper>
+    </footer>
   );
 };
 
