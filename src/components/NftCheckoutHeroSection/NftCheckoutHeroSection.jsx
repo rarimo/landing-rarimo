ee0244acafac04ad6f2c469287e4f752177e02a4
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2023-06-06 11:35:14 +0300

    nft hero section

--- src/components/NftCheckoutHeroSection/NftCheckoutHeroSection.jsx
+++ src/components/NftCheckoutHeroSection/NftCheckoutHeroSection.jsx
@@ -5,17 +5,23 @@ import { Trans, useTranslation } from 'react-i18next';
 import AppButton, { APP_BUTTON_SCHEMES } from '@/components/AppButton';
 import PartnersList from '@/components/PartnersList';
 import { CONFIG } from '@/config';
+import { COMPONENT_NODE_IDS } from '@/const';
 import { supportedBlockchainsList } from '@/template-data';
 
 const NftCheckoutHeroSection = () => {
   const { t } = useTranslation();
 
   return (
-    <section className="nft-checkout-hero-section">
-      <div className="nft-checkout-hero-section__content container">
-        <div className="nft-checkout-hero-section__hero-wrapper">
+    <section
+      id={COMPONENT_NODE_IDS.heroSection}
+      className="nft-checkout-hero-section"
+    >
+      <div className="nft-checkout-hero-section__inner">
+        <div className="nft-checkout-hero-section__content container">
           <h1 className="nft-checkout-hero-section__title">
-            {t('nft-checkout-hero-section.title')}
+            <span className="js-character-animation">
+              {t('nft-checkout-hero-section.title')}
+            </span>
           </h1>
           <p
             className="nft-checkout-hero-section__description"
@@ -23,6 +29,12 @@ const NftCheckoutHeroSection = () => {
           >
             <Trans i18nKey="nft-checkout-hero-section.description_html" />
           </p>
+          <PartnersList
+            className="nft-checkout-hero-section__blockchains"
+            titleKey="nft-checkout-hero-section.blockchains-title"
+            items={supportedBlockchainsList}
+            data-aos="fade-up"
+          />
           <div
             className="nft-checkout-hero-section__links-wrapper"
             data-aos="fade-up"
@@ -49,28 +61,6 @@ const NftCheckoutHeroSection = () => {
             />
           </div>
         </div>
-
-        <div className="nft-checkout-hero-section__blockchains-wrapper">
-          <PartnersList
-            titleKey="nft-checkout-hero-section.blockchains-title"
-            items={supportedBlockchainsList}
-            data-aos="fade-up"
-          />
-        </div>
-      </div>
-
-      <div
-        className="nft-checkout-hero-section__scroll-for-more"
-        data-aos="fade-up"
-      >
-        <svg
-          className="nft-checkout-hero-section__scroll-for-more-icon"
-          height="13"
-          width="13"
-        >
-          <use href="/icons/sprite.svg#icon-arrow-right"></use>
-        </svg>
-        <span>{t('nft-checkout-hero-section.scroll-for-more')}</span>
       </div>
     </section>
   );
--- src/const/component-node-ids.const.js
+++ src/const/component-node-ids.const.js
@@ -1,6 +1,6 @@
 export const COMPONENT_NODE_IDS = {
   application: 'application',
-  homeHeroSection: 'home-hero-section',
+  heroSection: 'hero-section',
   useCasesSection: 'use-cases-section',
   communitySection: 'community-section',
 };
