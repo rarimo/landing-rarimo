2a9bbcbff40c731778a372c4d3d9ca6caf5660f6
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2023-06-11 22:08:33 +0300

    removed app context

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,4 +1,3 @@
-export * from './get-scrollbar-width.helper';
 export * from './get-shifted-delay.helper';
 export * from './handle-nav-click.helper';
 export * from './prepare-aos-animation.helper';
