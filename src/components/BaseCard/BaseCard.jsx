ad4873ce4b7536ce065777c5b6b54f9732ad8d74
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2023-06-27 16:59:08 +0300

    added card's scale

--- src/components/BaseCard/BaseCard.jsx
+++ src/components/BaseCard/BaseCard.jsx
@@ -1,27 +1,31 @@
 import './BaseCard.scss';
 
 import cn from 'classnames';
+import { forwardRef } from 'react';
 
-const BaseCard = ({ children, className, isSection = false, tag }) => {
-  const CustomTag = tag || 'div';
+const BaseCard = forwardRef(
+  ({ children, className, isSection = false, tag }, ref) => {
+    const CustomTag = tag || 'div';
 
-  return (
-    <CustomTag
-      className={cn([
-        'base-card',
-        className,
-        {
-          'base-card--is-section': isSection,
-        },
-      ])}
-    >
-      <div className="base-card__bg-rect">
-        <div className="base-card__content">
-          <>{children}</>
+    return (
+      <CustomTag
+        ref={ref}
+        className={cn([
+          'base-card',
+          className,
+          {
+            'base-card--is-section': isSection,
+          },
+        ])}
+      >
+        <div className="base-card__bg-rect">
+          <div className="base-card__content">
+            <>{children}</>
+          </div>
         </div>
-      </div>
-    </CustomTag>
-  );
-};
+      </CustomTag>
+    );
+  },
+);
 
 export default BaseCard;
