174df1594c72a00ea53354e1c8b72c4fd6f26462
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2023-06-28 23:33:15 +0300

    created component for spotlight

--- src/components/HomeHeroSection/HomeHeroSection.jsx
+++ src/components/HomeHeroSection/HomeHeroSection.jsx
@@ -1,10 +1,11 @@
 import './HomeHeroSection.scss';
 
-import { lazy, useEffect, useRef } from 'react';
+import { lazy } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import AppButton, { APP_BUTTON_SCHEMES } from '@/components/AppButton';
 import PartnersList from '@/components/PartnersList';
+import SpotlightBg from '@/components/SpotlightBg';
 import { CONFIG } from '@/config';
 import { COMPONENT_NODE_IDS, ROUTES_PATHS } from '@/const';
 import useAppContext from '@/hooks/useAppContext';
@@ -12,54 +13,12 @@ import { supportedBlockchainsList } from '@/template-data';
 
 const HomeHeroDecor = lazy(() => import('@/components/HomeHeroDecor'));
 
-let onMousemove;
-let sectionRect;
-
 const HomeHeroSection = () => {
   const { t } = useTranslation();
   const { isDesktop } = useAppContext();
 
-  const sectionRef = useRef(null);
-  const spotlightRef = useRef(null);
-
-  useEffect(() => {
-    const removeListeners = () => {
-      window.removeEventListener('mousemove', onMousemove);
-    };
-
-    if (isDesktop) {
-      sectionRect = sectionRef.current.getBoundingClientRect();
-
-      const onMousemove = ({ pageX, pageY }) => {
-        if (!spotlightRef.current || window.scrollY > sectionRect.height)
-          return;
-
-        const x = pageX;
-        const y = pageY;
-        const spotlightSize = 'transparent 80px, rgba(0, 0, 0, 0.8) 200px)';
-
-        spotlightRef.current.style.backgroundImage = `radial-gradient(circle at ${x}px ${y}px, ${spotlightSize}`;
-      };
-
-      document.addEventListener('mousemove', onMousemove);
-      return;
-    }
-
-    if (!isDesktop) {
-      removeListeners();
-    }
-
-    return () => {
-      removeListeners();
-    };
-  }, [isDesktop]);
-
   return (
-    <section
-      id={COMPONENT_NODE_IDS.heroSection}
-      ref={sectionRef}
-      className="home-hero-section"
-    >
+    <section id={COMPONENT_NODE_IDS.heroSection} className="home-hero-section">
       <div className="home-hero-section__content container">
         {isDesktop && <HomeHeroDecor className="home-hero-section__decor" />}
 
@@ -112,10 +71,7 @@ const HomeHeroSection = () => {
           />
         </div>
       </div>
-      <div className="home-hero-section__bg-rect-backdrop" />
-      {isDesktop && (
-        <div ref={spotlightRef} className="home-hero-section__spotlight" />
-      )}
+      <SpotlightBg />
     </section>
   );
 };
