c27e2e08ad5bd1dfe1d51313ef5aab10aa547cda
nikitabelous <nikitabelous16@gmail.com>
2023-07-04 17:19:07 +0300

    fix header

--- src/template-data/navigation.js
+++ src/template-data/navigation.js
@@ -3,21 +3,15 @@ import { COMPONENT_NODE_IDS, ROUTES_PATHS } from '@/const';
 
 export const navigation = [
   {
-    textKey: 'navigation.home',
-    path: COMPONENT_NODE_IDS.heroSection,
-    hash: true,
-    routeLink: ROUTES_PATHS.home,
-  },
-  {
     textKey: 'navigation.use-cases',
     path: COMPONENT_NODE_IDS.useCasesSection,
     hash: true,
     includeRoutes: [ROUTES_PATHS.home],
   },
   {
-    textKey: 'navigation.community',
-    path: COMPONENT_NODE_IDS.communitySection,
-    hash: true,
+    textKey: 'navigation.documentation',
+    path: CONFIG.docsLink,
+    external: true,
   },
   {
     textKey: 'navigation.support',
