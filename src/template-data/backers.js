c03b7cac45d2009e7cda9a123169df0ef1bab63c
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2023-07-10 11:50:22 +0300

    refactor

--- src/template-data/backers.js
+++ src/template-data/backers.js
@@ -3,16 +3,19 @@ export const backersList = [
     image: '/img/backers/pareto.webp',
     width: 69,
     height: 10,
+    withOpacity: true,
   },
   {
     image: '/img/backers/greycroft.webp',
     width: 72,
     height: 16,
+    withOpacity: true,
   },
   {
     image: '/img/backers/eniac.webp',
     width: 53,
     height: 17,
+    withOpacity: true,
   },
   {
     image: '/img/backers/pantera.webp',
@@ -23,15 +26,18 @@ export const backersList = [
     image: '/img/backers/hyper.png',
     width: 62,
     height: 12,
+    withOpacity: true,
   },
   {
     image: '/img/backers/circle.webp',
     width: 69,
     height: 21,
+    withOpacity: true,
   },
   {
     image: '/img/backers/protocol-labs.webp',
     width: 91,
     height: 24,
+    withOpacity: true,
   },
 ];
