74a7bfa4a1f8c0c109789beb4366113f477f828e
nikitabelous <nikitabelous16@gmail.com>
2023-07-18 21:06:54 +0300

    Revert "change img to png"
    This reverts commit 743867f61c8f29999b94035a993c451db12f65e4.

--- src/template-data/backers.js
+++ src/template-data/backers.js
@@ -1,35 +1,35 @@
 export const backersList = [
   {
-    image: '/img/backers/greycroft.png',
+    image: '/img/backers/greycroft.webp',
     width: 124,
     height: 27,
     withOpacity: false,
   },
   {
-    image: '/img/backers/pantera.png',
+    image: '/img/backers/pantera.webp',
     width: 208,
     height: 16,
     withOpacity: false,
   },
   {
-    image: '/img/backers/eniac.png',
+    image: '/img/backers/eniac.webp',
     width: 80,
     height: 26,
     withOpacity: false,
   },
   {
-    image: '/img/backers/hyper.png',
+    image: '/img/backers/hyper.webp',
     width: 119,
     height: 48,
   },
   {
-    image: '/img/backers/circle.png',
+    image: '/img/backers/circle.webp',
     width: 132,
     height: 40,
     withOpacity: false,
   },
   {
-    image: '/img/backers/protocol-labs.png',
+    image: '/img/backers/protocol-labs.webp',
     width: 214,
     height: 32,
     withOpacity: false,
