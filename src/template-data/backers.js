743867f61c8f29999b94035a993c451db12f65e4
nikitabelous <nikitabelous16@gmail.com>
2023-07-18 20:55:18 +0300

    change img to png

--- src/template-data/backers.js
+++ src/template-data/backers.js
@@ -1,35 +1,35 @@
 export const backersList = [
   {
-    image: '/img/backers/greycroft.webp',
+    image: '/img/backers/greycroft.png',
     width: 124,
     height: 27,
     withOpacity: false,
   },
   {
-    image: '/img/backers/pantera.webp',
+    image: '/img/backers/pantera.png',
     width: 208,
     height: 16,
     withOpacity: false,
   },
   {
-    image: '/img/backers/eniac.webp',
+    image: '/img/backers/eniac.png',
     width: 80,
     height: 26,
     withOpacity: false,
   },
   {
-    image: '/img/backers/hyper.webp',
+    image: '/img/backers/hyper.png',
     width: 119,
     height: 48,
   },
   {
-    image: '/img/backers/circle.webp',
+    image: '/img/backers/circle.png',
     width: 132,
     height: 40,
     withOpacity: false,
   },
   {
-    image: '/img/backers/protocol-labs.webp',
+    image: '/img/backers/protocol-labs.png',
     width: 214,
     height: 32,
     withOpacity: false,
