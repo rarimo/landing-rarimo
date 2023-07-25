534cf97869e11df96fb33d933e9d742557025185
nikitabelous <nikitabelous16@gmail.com>
2023-07-25 14:02:42 +0300

    v1 animation

--- src/pages/NftCheckoutPage/NftCheckoutPage.jsx
+++ src/pages/NftCheckoutPage/NftCheckoutPage.jsx
@@ -5,13 +5,15 @@ import CommunitySection from '@/components/CommunitySection';
 import NftCheckoutHeroSection from '@/components/NftCheckoutHeroSection';
 import NftCheckoutStepsSection from '@/components/NftCheckoutStepsSection';
 import SubscribeSection from '@/components/SubscribeSection';
+import NftCheckoutBlockAnimationSection from "@/components/NftCheckoutBlockAnimationSection";
 
 const NftCheckoutPage = () => {
   return (
     <>
       <div className="nft-checkout-page__animation-container">
         <NftCheckoutHeroSection />
-        <NftCheckoutStepsSection />
+        {/*<NftCheckoutStepsSection />*/}
+        <NftCheckoutBlockAnimationSection/>
       </div>
       <AdvantagesSection />
       <CommunitySection />
