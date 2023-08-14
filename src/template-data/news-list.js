180fed4ade92377930ec68b5a8b1cf9394fe84c6
nikitabelous <nikitabelous16@gmail.com>
2023-08-14 18:05:00 +0300

    feat: redesign news section

--- src/template-data/news-list.js
+++ src/template-data/news-list.js
@@ -1,18 +1,38 @@
 export const newsList = [
   {
+    blockName: 'news-section.first-block-name',
     textKey: 'news-section.first-title',
     link: 'https://blockworks.co/news/rarimo-polygon-id-ethereum',
   },
   {
+    blockName: 'news-section.second-block-name',
     textKey: 'news-section.second-title',
     link: 'https://cointelegraph.com/news/nft-marketplace-enables-cross-chain-purchases-on-multiple-networks',
   },
   {
+    blockName: 'news-section.third-block-name',
     textKey: 'news-section.third-title',
     link: 'https://www.theblock.co/post/239028/ai-blockchain-synergy-may-hold-the-answer-to-protecting-drakes-voice',
   },
   {
+    blockName: 'news-section.fourth-block-name',
     textKey: 'news-section.fourth-title',
     link: 'https://www.youtube.com/watch?v=y-ymBN8P9Nc',
+  },
+  {
+    textKey: 'news-section.five-title',
+    link: 'https://cointelegraph.com/news/nft-marketplace-enables-cross-chain-purchases-on-multiple-networks',
+  },
+  {
+    textKey: 'news-section.six-title',
+    link: 'https://rarimo.medium.com/what-is-interoperability-and-why-is-it-so-important-6b421bdaab09',
+  },
+  {
+    textKey: 'news-section.seven-title',
+    link: 'https://www.veradiverdict.com/p/rarimoo',
+  },
+  {
+    textKey: 'news-section.eighth-title',
+    link: 'https://rarimo.notion.site/Self-sovereign-identities-SSI-982f1b0989774289af73d54b8a76e426',
   },
 ];
