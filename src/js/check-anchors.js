d3e0cde44a90767cce54613534ad5461f7245477
Nikita Belous <nikitabelous16@gmail.com>
2023-09-28 09:40:10 +0000

    Feat/add anchors

---
+++ src/js/check-anchors.js
@@ -0,0 +1,16 @@
+import { COMPONENT_NODE_IDS } from '@/const';
+
+export const checkAnchors = hash => {
+  switch (hash) {
+    case '#' + COMPONENT_NODE_IDS.application:
+      return COMPONENT_NODE_IDS.application;
+    case '#' + COMPONENT_NODE_IDS.communitySection:
+      return COMPONENT_NODE_IDS.application;
+    case '#' + COMPONENT_NODE_IDS.useCasesSection:
+      return COMPONENT_NODE_IDS.useCasesSection;
+    case '#' + COMPONENT_NODE_IDS.heroSection:
+      return COMPONENT_NODE_IDS.heroSection;
+    default:
+      return;
+  }
+};
