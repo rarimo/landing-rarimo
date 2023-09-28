53d4d370d8a1338f59c320f9b2206fb8688c061b
Nikita Belous <nikitabelous16@gmail.com>
2023-09-28 09:40:10 +0000

    Feat/add anchors

--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -1,14 +1,23 @@
-import { memo } from 'react';
+import { memo, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 
 import AppBar from '@/components/AppBar';
 import AppFooter from '@/components/AppFooter';
 import RouteTransition from '@/components/RouteTransition';
+import useNavigation from '@/hooks/useNavigation';
+import { checkAnchors } from '@/js';
 
 const MainLayout = () => {
   const { t } = useTranslation();
+  const location = useLocation();
+  const { handleNavClick } = useNavigation();
 
+  useEffect(() => {
+    if (location.hash) {
+      handleNavClick({ hash: true, path: checkAnchors(location.hash) });
+    }
+  }, []);
   return (
     <>
       <AppBar />
