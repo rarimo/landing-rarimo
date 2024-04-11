e9bbf1bc3bef018719c7fd2179ad501cba7f53ae
ardier16 <ardier16@gmail.com>
2024-04-11 13:12:27 +0300

    Add protocol section

---
+++ src/components/ProtocolSection/ProtocolSection.jsx
@@ -0,0 +1,57 @@
+import './ProtocolSection.scss';
+
+import useAppContext from '@/hooks/useAppContext';
+
+const ProtocolSection = () => {
+  const { isDesktop } = useAppContext();
+
+  return (
+    <section className="protocol-section">
+      <div className="container">
+        <div className="protocol-section__title-wrp" data-aos="fade-up">
+          <h2 className="protocol-section__title">Protocol layer</h2>
+          <p className="protocol-section__text">
+            The protocol acts as an identity state broadcasting network,
+            bridging Identity authentication and private social graph
+            interactions across blockchains
+          </p>
+        </div>
+
+        <div className="protocol-section__content" data-aos="fade-up">
+          <div className="protocol-section__img-wrp">
+            <img
+              className="protocol-section__img"
+              src={
+                isDesktop
+                  ? '/img/home/protocol.png'
+                  : '/img/home/protocol-mobile.png'
+              }
+              alt="Protocol Layer"
+            />
+          </div>
+          <div className="protocol-section__list">
+            <p className="protocol-section__list-item">Decentralized</p>
+            <p className="protocol-section__list-item">
+              POS with Instant finality
+            </p>
+            <p className="protocol-section__list-item">
+              Privacy via Zero Knowledge Proofs
+            </p>
+            <div className="protocol-section__list-item protocol-section__list-item--large">
+              <span>Efficiency through</span>
+              <div className="protocol-section__list-item-line" />
+              <span className="protocol-section__list-sub-item">
+                Identity state aggregation
+              </span>
+              <span className="protocol-section__list-sub-item">
+                On-demand replication
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ProtocolSection;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,6 +2,7 @@ import CommunitySection from '@/components/CommunitySection';
 import ConfidentialIdentitySection from '@/components/ConfidentialIdentitySection';
 import HomeHeroSection from '@/components/HomeHeroSection';
 import NewsSection from '@/components/NewsSection';
+import ProtocolSection from '@/components/ProtocolSection';
 import SubscribeSection from '@/components/SubscribeSection';
 import UseCasesSection from '@/components/UseCasesSection';
 
@@ -18,9 +19,5 @@ const HomePage = () => {
     </>
   );
 };
-
-function ProtocolSection() {
-  return <div />;
-}
 
 export default HomePage;
