83d8b07c12a79b17bca646628f61309fff200e3a
lukachi <lukachisama@gmail.com>
2025-02-19 13:30:35 +0200

    add load more btn wip

--- src/components/Blog/components/Articles/index.tsx
+++ src/components/Blog/components/Articles/index.tsx
@@ -1,3 +1,5 @@
+import { ChevronDown } from 'lucide-react'
+
 import Filters from './components/Filters'
 import List from './components/List'
 
@@ -6,6 +8,13 @@ export default function Articles() {
     <div className='flex flex-col'>
       <Filters className='mt-10' />
       <List className='mt-10' />
+
+      <button className='mx-auto mt-14 flex items-center gap-1'>
+        <span className='text-textSecondary typography-buttonMedium'>
+          Show more
+        </span>
+        <ChevronDown className={'size-4 text-textSecondary'} />
+      </button>
     </div>
   )
 }
--- src/components/Blog/index.tsx
+++ src/components/Blog/index.tsx
@@ -1,4 +1,5 @@
 import { cn } from '@/theme/utils'
+import { UiHorizontalDivider } from '@/ui'
 
 import Articles from './components/Articles'
 import HeroSection from './components/HeroSection'
@@ -9,6 +10,8 @@ export default function Blog() {
       <div className='mx-auto flex max-w-[1136px] flex-col'>
         <HeroSection className='px-0 py-0 md:px-0 md:py-0' />
         <Articles />
+
+        <UiHorizontalDivider className='mt-14' />
       </div>
     </div>
   )
