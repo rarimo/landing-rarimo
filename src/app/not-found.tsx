ef623533233178e07dea8707cea69ff4d65f7279
lukachi <lukachisama@gmail.com>
2025-02-21 17:42:23 +0200

    Update 404 page, API cache duration, and layout styles
    Redesigned the 404 page with improved UI and user navigation. Adjusted learning hub API cache invalidation duration from 1 hour to 60 seconds. Refined layout styles for better alignment and removed unused GitHub Actions branch.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,41 @@
+import Link from 'next/link'
+
+import { cn } from '@/theme/utils'
+
+import UiContainer from '../ui/UiContainer'
+
 export default function NotFound() {
-  return <div>Custom 404 Page</div>
+  return (
+    <UiContainer
+      showGradientDecor
+      gradientDecorClassName={cn(
+        '-right-[275px] -top-[205px] -rotate-[10deg] h-[367px] w-[438px]',
+        'md:-right-[105px] md:-top-[225px] md:-rotate-[10deg] md:h-[367px] md:w-[438px]',
+      )}
+      className={cn(
+        'flex flex-col items-center justify-center bg-backgroundContainer px-4',
+      )}
+    >
+      <div className='mx-auto max-w-screen-xl px-4 py-8 lg:px-6 lg:py-16'>
+        <div className='mx-auto max-w-screen-sm text-center'>
+          <h1 className='text-primary mb-4 !font-extrabold !tracking-tight typography-h1 lg:text-9xl'>
+            404
+          </h1>
+          <p className='mb-4 text-3xl font-bold tracking-tight text-textPrimary md:text-4xl'>
+            Something's missing.
+          </p>
+          <p className='mb-4 text-lg font-light text-textSecondary'>
+            Sorry, we can't find that page. You'll find lots to explore on the
+            home page.
+          </p>
+          <Link
+            href='/'
+            className='my-4 inline-flex rounded-lg bg-componentPrimary px-5 py-2.5 text-center text-sm font-medium text-textPrimary hover:ring-textPrimary focus:outline-none focus:ring-4'
+          >
+            Back to Homepage
+          </Link>
+        </div>
+      </div>
+    </UiContainer>
+  )
 }
--- src/components/LearningHub/components/LearningHubPosts/components/List.tsx
+++ src/components/LearningHub/components/LearningHubPosts/components/List.tsx
@@ -14,12 +14,12 @@ export default function List({ posts, className, ...rest }: Props) {
   return (
     <div
       {...rest}
-      className={cn('flex flex-wrap justify-between gap-6', className)}
+      className={cn('flex flex-wrap justify-start gap-6', className)}
     >
       {posts.map((el, idx) => (
         <Item
           key={idx}
-          className={cn('w-full', 'sm:w-[45%]', 'lg:w-[31%]')}
+          className={cn('w-full', 'sm:w-[47%]', 'lg:w-[31.5%]')}
           id={String(el.id)}
           title={el.attributes.title}
           desc={el.attributes.shortDescription}
